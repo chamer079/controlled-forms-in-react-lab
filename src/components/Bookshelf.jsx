bd55d01a96b2177082a2e17c2ffe061e8c227b67
chamer79 <chamer79@gmail.com>
2024-11-04 14:06:38 -0500

    completed exercise 3.2

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 const Bookshelf = () => {
-    const [books, setBook] = useState([])
+    const [books, setBooks] = useState([])
     // console.log(books)
     const [newBooks, setNewBooks] = useState({
         title: "",
@@ -9,11 +9,21 @@ const Bookshelf = () => {
     })
     // console.log(newBooks)
 
+    //This function updates the form’s state as the user types into the input fields
     const handleInputChange = (e) => {
         console.log(e.target.name)
 
         setNewBooks({...newBooks, [e.target.name] : e.target.value})
     }
+
+    // This function manages the submission of the form, adding a new book to the list and resetting the input fields.
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        setBooks({...books})
+        setBooks("")
+    }
+
 
     return(
         <div className="bookshelfDiv">
